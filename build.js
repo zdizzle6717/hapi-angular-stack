948b46ae521aa27b5cbba79531bf72d794e0ee93
Zack Anselm <zanselm5@gmail.com>
2016-08-28 14:48:18 +0000

    Updated build tool and added autoprefixer to css

--- build.js
+++ build.js
@@ -3,6 +3,9 @@
 var fs = require("fs");
 var browserify = require("browserify");
 var stringify = require("stringify");
+const sass = require('node-sass');
+const autoPrefixer = require('autoprefixer');
+const autoPrefix = require('postcss')([autoPrefixer]);
 
 /* Build Main App*/
 browserify("app/app.js")
@@ -14,3 +17,25 @@ browserify("app/app.js")
     })
     .bundle()
     .pipe(fs.createWriteStream("dist/js/app.js"));
+
+// Compile SCSS
+sass.render({
+    file: 'app/styles/app.scss',
+    outputStyle: 'compressed'
+}, (err, result) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    autoPrefix.process(result.css.toString())
+        .then((result) => {
+            let dataString = result.css.toString();
+            let kbs = Buffer.byteLength(dataString) / 1000;
+
+            result.warnings().forEach(function(warn) {
+                console.warn(warn.toString());
+            });
+            fs.writeFileSync('dist/css/app.css', dataString, 'utf8');
+        });
+});
